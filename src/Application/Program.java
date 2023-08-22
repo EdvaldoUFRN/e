51a13bdca88413573ca9cb63f91405da39071c15
EdvaldoUFRN <edvaldo_junior2009@hotmail.com>
2023-08-22 11:59:46 -0300

    Exercise 1 Completed

---
+++ src/Application/Program.java
@@ -0,0 +1,27 @@
+package Application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double largura = sc.nextDouble();
+		double comprimento = sc.nextDouble();
+		double metroquadrado = sc.nextDouble();
+		
+		double area = largura * comprimento;
+		double preco = area * metroquadrado;
+		
+		System.out.printf("Area = %.2f%n", area);
+		System.out.printf("Preço = %.2f%n", preco);
+		
+		
+		sc.close();
+	}
+
+}
